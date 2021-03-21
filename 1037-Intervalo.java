161919662fb8507c66f9c0010a7f52d04ebb051e
William Marques <matrix.consultoria@gmail.com>
2021-03-21 01:01:57 +0000

    add 1037

---
+++ 1037-Intervalo.java
@@ -0,0 +1,21 @@
+/*import java.util.Scanner;
+
+class Main{
+  public static void main(String[]args){
+    double n;
+
+    Scanner teclado = new Scanner(System.in);
+    n = teclado.nextDouble();
+    if (n >= 0 && n <= 25.0000){
+      System.out.printf("Intervalo [0,25]\n");
+    } else if (n >= 25.00001 && n <= 50.0000000){
+      System.out.printf("Intervalo (25,50]\n");
+    } else if (n >= 50.00001 && n <= 75.0000000){
+      System.out.printf("Intervalo (50,75]\n");
+    } else if (n >= 75.00001 && n <= 100.0000000){
+      System.out.printf("Intervalo (75,100]\n");
+    } else {
+      System.out.printf("Fora de intervalo\n");
+      }
+  }
+}*/
--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ class Main{
   public static void main(String[]args){
 
     Scanner teclado = new Scanner(System.in);
+    
   }
-
 }
